b6b37d0ffcdf156b515709ae4ecbeb059b2890fa
Friigy <bastian.szczygielski@gmail.com>
2018-12-30 21:03:27 +0100

    clean up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-import NavBar from './components/NavBar';
 import FiMaidRouter from './components/FiMaidRouter';
 
 class App extends Component {
@@ -15,20 +12,3 @@ class App extends Component {
 }
 
 export default App;
-
-/*
-<header className="App-header">
-  <img src={logo} className="App-logo" alt="logo" />
-  <p>
-    Edit <code>src/App.js</code> and save to reload.
-  </p>
-  <a
-    className="App-link"
-    href="https://reactjs.org"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    Learn React
-  </a>
-</header>
-*/
